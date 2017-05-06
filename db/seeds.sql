91330730979f2f63001a21827fde1792af49fc05
Katharina Fuetterer <Katharina Fuetterer>
2017-05-05 21:03:49 -0400

    Update 05-05-17 903

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Son of a Bun', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baby Got Bok Choy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rings of Fire', false);
