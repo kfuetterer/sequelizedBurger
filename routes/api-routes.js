19824ac585f16b1ac6225e0a1391da41b70a74fc
Katharina Fuetterer <Katharina Fuetterer>
2017-05-12 16:49:48 -0400

    Update 05-12

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,37 +1,34 @@
-var express = require("express");
-var router = express.Router();
-
 var db = require("../models");
 
-router.get("/", function(req, res) {
-  db.Burgers.findAll({}).then(function(results) {
-    var hbsObject = {
-      burgers: results
-    };
-    res.render("index", hbsObject);
+module.exports = function(app){
+  app.get("/", function(req, res) {
+    db.Burger.findAll({}).then(function(results) {
+      var hbsObject = {
+        burgers: results
+      };
+      res.render("index", hbsObject);
+    });
   });
-});
 
-router.post("/", function(req, res) {
-  db.Burgers.create({
-    burger_name: req.body.burger_name,
-    devoured: false
-  }).then(function(results) {
-    res.end();
-  });
-});
-
-router.put("/:id", function(req, res) {
-  db.Burgers.update(
-    {
-      devoured: true
-    },{
-      where: {
-        id: req.params.id
-      }
+  app.post("/", function(req, res) {
+    db.Burger.create({
+      burger_name: req.body.burger_name,
+      devoured: false
     }).then(function(results) {
-      res.end();
-    })
-});
+      res.redirect("/");
+    });
+  });
 
-module.exports = router;
+  app.put("/:id", function(req, res) {
+    db.Burger.update(
+      {
+        devoured: true
+      },{
+        where: {
+          id: req.params.id
+        }
+      }).then(function(results) {
+        res.redirect("/");
+      });
+  });
+};
--- server.js
+++ server.js
@@ -3,17 +3,26 @@ var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var port = 3000;
+var db = require("./models");
+
 var app = express();
+var PORT = process.env.NODE_ENV || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(methodOverride("_method"));
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
+
+require("./routes/api-routes.js")(app);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
-
-app.use("/", routes);
-app.listen(port);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
